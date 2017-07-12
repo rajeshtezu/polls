880eb76c7aedbb3485d9511738a66640258e4b28
rajsourcelair <rajthebest9000@gmail.com>
2017-07-12 07:35:03 +0000

    index modified

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,15 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # /polls/
+    url(r'^$', views.index, name='index'),
+
+    # /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /polls/5/result
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+
+    # /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
