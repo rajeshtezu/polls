22f9a832d15e3a373d852ee6958ec65ff4c346b0
Rajesh Kumar <rajeshtezu90@gmail.com>
2017-07-16 16:04:38 +0530

    customize admin form-2

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,19 @@
 from .models import Question, Choice
 from django.contrib import admin
 
+
+class ChoiceInline(admin.StackedInline):
+	model = Choice
+	extra = 3
+	
+		
 class QuestionAdmin(admin.ModelAdmin):
-	fields = ['pub_date', 'question_text']
+	fieldsets = [
+		(None, {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
 
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+#admin.site.register(Choice)
